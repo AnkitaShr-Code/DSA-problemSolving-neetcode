a39d7b73ed7118314790efb78e04237a55266f59
AnkitaShr-Code <132663295+AnkitaShr-Code@users.noreply.github.com>
2026-05-21 22:07:27 +0530

    Add: power-of-two - submission-0

---
+++ Data Structures & Algorithms/power-of-two/submission-0.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {boolean}
+     */
+    isPowerOfTwo(n) {
+        if(n == 1) 
+            return true
+        else if (n < 1 || n %2 !== 0) {
+            return false;
+        }
+
+        return this.isPowerOfTwo(n/2);
+    }
+}
