45e84657e92ebf551c2a24bff7e7218ea1aec067
AnkitaShr-Code <132663295+AnkitaShr-Code@users.noreply.github.com>
2026-05-30 12:01:49 +0530

    Add: middle-of-the-linked-list - submission-3

---
+++ Data Structures & Algorithms/middle-of-the-linked-list/submission-3.ts
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {ListNode}
+     */
+    middleNode(head: ListNode | null): ListNode {
+        let fast: ListNode | null = head;
+        let slow: ListNode | null = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+        }
+        return slow;
+    }
+}
