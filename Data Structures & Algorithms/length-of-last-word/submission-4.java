1020b5787be6bba5f88e28201b2bb0190f26200d
AnkitaShr-Code <132663295+AnkitaShr-Code@users.noreply.github.com>
2026-06-20 11:53:36 +0530

    Add: length-of-last-word - submission-4

---
+++ Data Structures & Algorithms/length-of-last-word/submission-4.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int n = s.length() - 1;
+
+        while(n >= 0) {
+            if(s.charAt(n) != ' '){
+                break;
+            }
+            n--;
+        }
+        
+        int count = 0;
+        while(n >= 0 && s.charAt(n) != ' '){
+            count++;
+            n--;
+        }
+        return count;
+    }
+}
