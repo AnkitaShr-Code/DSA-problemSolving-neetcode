1700048ddefb282287e48a3b8ab6c2fb13b9ea03
Ankita Sharma <132663295+AnkitaShr-Code@users.noreply.github.com>
2026-08-22 11:59:24 +0530

    Add: three-integer-sum - submission-0

---
+++ Data Structures & Algorithms/three-integer-sum/submission-0.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        nums.sort((a,b) => a-b);
+        const result = [];
+        let n = nums.length;
+
+        for(let i =0; i<n-2; i++) {
+            if(i > 0 && nums[i] === nums[i - 1]) continue;
+            if(nums[i] > 0) break;
+
+            let left = i + 1;
+            let right = n -1;
+            while ( left < right) {
+                if(nums[i] + nums[left] + nums[right] == 0) {
+                    result.push([nums[i], nums[left], nums[right]]);
+                    while (left < right && nums[left] === nums[left + 1]) 
+                        left++;
+                    while (left < right && nums[right] === nums[right - 1]) 
+                        right--;
+                    left++;
+                    right--;
+                } else if(nums[i] + nums[left] + nums[right] > 0) {
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+        }
+        return result;
+    }
+}
